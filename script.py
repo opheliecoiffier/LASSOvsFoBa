1883a49ce38bc918a37416848604447c636a10f9
opheliecoiffier <60701868+opheliecoiffier@users.noreply.github.com>
2020-11-10 16:40:38 +0100

    lasso_alpha_coefficient

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ from sklearn.datasets import load_boston
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
-from source import train_error_data, test_error_data
+from source import train_error_data, test_error_data, alpha_choice, alpha_choice_fig
 
 
 ##########################
@@ -34,16 +34,27 @@ n = 50
 J = 10
 train_size = 50
 test_size = 301
+my_alphas = np.array([0.004,0.006,0.01,0.05,0.1,0.15, 0.2, 0.25, 0.3, 0.4])
+
 
 fig = plt.figure() 
-train_error_data(n, J, x, y, train_size)
+train_error_data(n, J, x, y, train_size, nb_features=33, my_alphas=my_alphas)
 fig.savefig("training_error_ionosphere.pdf")
 
+
 fig2 = plt.figure()
-test_error_data(n, J, x, y, test_size, train_size)
+test_error_data(n, J, x, y, test_size, train_size, nb_features=33, my_alphas=my_alphas)
 fig2.savefig("test_error_ionosphere.pdf")
 
-#%%
+####################################
+#Choice of alpha for Lasso method
+#----------------------------------
+fig_alpha1 = plt.figure()
+alpha_choice_fig(x, y, my_alphas, nb_features=33, train_size=train_size)
+fig_alpha1.savefig('alpha_choice_lasso')
+print(alpha_choice(x, y, my_alphas, nb_features=33, train_size=train_size))
+
+
 #############################
 #import data Boston housing
 #############################
@@ -60,11 +71,23 @@ n = 50 #number of repetition
 J = 10 #number of sparsity
 train_size = 50
 test_size = 456
+my_alphas_house = np.array([0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0])
 
+#Training points
 fig3 = plt.figure()
-train_error_data(n, J, X, Y, train_size)
+train_error_data(n, J, X, Y, train_size, nb_features=13, my_alphas=my_alphas_house)
 fig3.savefig("training_error_housing.pdf")
 
+#Test points
 fig4 = plt.figure()
-test_error_data(n, J, X, Y, test_size, train_size)
+test_error_data(n, J, X, Y, test_size, train_size, nb_features=13, my_alphas=my_alphas_house)
 fig4.savefig("test_error_housing.pdf")
+
+####################################
+#Choice of alpha for Lasso method
+#----------------------------------
+fig_alpha2 = plt.figure()
+alpha_choice_fig(X, Y, my_alphas_house, nb_features=13, train_size=train_size)
+fig_alpha2.savefig('alpha_choice_lasso_house')
+print(alpha_choice(X, Y, my_alphas_house, nb_features=13, train_size=train_size))
+
--- source.py
+++ source.py
@@ -1,15 +1,58 @@
 #packages used
-from sklearn.linear_model import Lasso, OrthogonalMatchingPursuit, Lars
+from sklearn.linear_model import Lasso, OrthogonalMatchingPursuit, Lars, lasso_path
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+############################
+#Alpha coefficients : Lasso
+#############################
+def alpha(X_train, train_size, nb_features, my_alphas):
+    '''
+
+    Parameters
+    ----------
+    X_train : data training points.
+    train_size : number of training points.
+    nb_features : number of features.
+    my_alphas : array of different values for alpha.
+
+    Returns : array of different values for alpha for Lasso method : 
+              opposite order of my_alphas array.
+    -------
+    None.
+
+    '''
+    alpha_for_path, coefs_lasso, _ = lasso_path(X_train[:,0:nb_features-1],
+                                                X_train[:,nb_features-1],alphas=my_alphas)
+    return(alpha_for_path)
+
 
 ################################
 #Average train squared error
 ################################
 
-def train_error_data(n, J, x, y, train_size):
-    #initialisation : 3 methods (Lasso, Larsn OMP)
+def train_error_data(n, J, x, y, train_size, nb_features, my_alphas):
+    '''
+    
+    Parameters
+    ----------
+    n : number of repetitions.
+    J : number of sparsity.
+    x : data.
+    y : desired output.
+    train_size : number of training points.
+    nb_features : number of features.
+    my_alphas : array of different values for alpha.
+
+    Returns : representation of MSE depending on sparsity for Lasso, OMP and Lars methods,
+              for training points.
+    -------
+
+    '''
+    #initialisation
     vec = np.zeros(train_size*J).reshape(train_size,J)
     res = np.zeros(n*J).reshape(n,J)    
     somme = np.zeros(J)
@@ -25,12 +68,14 @@ def train_error_data(n, J, x, y, train_size):
     for i in range(n):
         X_train, X_test, y_train, y_test = train_test_split(x, y,train_size=train_size)
         for j in range(J):
-            reg2 = Lasso(alpha=1/(j+1)).fit(X_train, y_train)
+            alpha_coef = alpha(X_train, train_size=train_size, nb_features=nb_features, 
+                                     my_alphas=my_alphas)
+            reg2 = Lasso(alpha=alpha_coef[j]).fit(X_train, y_train)
             reg = OrthogonalMatchingPursuit(n_nonzero_coefs=j+1).fit(X_train, y_train)
             reg3 = Lars(n_nonzero_coefs=j+1).fit(X_train, y_train)
             vec[:,j] = (y_train-reg.predict(X_train))**2
             res[i,j] = sum(vec[:,j])/train_size
-            vec2[:,j] = (y_train-reg2.predict(X_train))**2
+            vec2[:,j] = (y_train-(reg2.predict(X_train)))**2
             res2[i,j] = sum(vec2[:,j])/train_size
             vec3[:,j] = (y_train-reg3.predict(X_train))**2
             res3[i,j] = sum(vec3[:,j])/train_size
@@ -58,8 +103,26 @@ def train_error_data(n, J, x, y, train_size):
 #Average test squared error
 ################################
 
-def test_error_data(n, J, x, y, test_size, train_size):
-    #initialisation : 3 methods (Lasso, Lars, OMP)
+def test_error_data(n, J, x, y, test_size, train_size, nb_features, my_alphas):
+    '''
+
+    Parameters
+    ----------
+    n : number of repetitions.
+    J : number of sparsity.
+    x : data.
+    y : desired output.
+    test_size : number of test points.
+    train_size : number of training points.
+    nb_features : number of features.
+    my_alphas : array of different values for alpha.
+
+    Returns : representation of MSE depending on sparsity for Lasso, OMP and Lars methods,
+              for test points.
+    -------
+
+    '''
+    #initialisation 
     vec = np.zeros(test_size*J).reshape(test_size,J)
     res = np.zeros(n*J).reshape(n,J)    
     somme = np.zeros(J)
@@ -75,7 +138,9 @@ def test_error_data(n, J, x, y, test_size, train_size):
     for i in range(n):
         X_train, X_test, y_train, y_test = train_test_split(x, y,train_size=train_size)
         for j in range(J):
-            reg2 = Lasso(alpha=1/(j+1)).fit(X_train, y_train)
+            alpha_coef = alpha(X_train, train_size=train_size, nb_features=nb_features, 
+                                     my_alphas=my_alphas)
+            reg2 = Lasso(alpha=alpha_coef[j]).fit(X_train, y_train)
             reg = OrthogonalMatchingPursuit(n_nonzero_coefs=j+1).fit(X_train, y_train)
             reg3 = Lars(n_nonzero_coefs=j+1).fit(X_train, y_train)
             vec[:,j] = (y_test-reg.predict(X_test))**2
@@ -103,3 +168,53 @@ def test_error_data(n, J, x, y, test_size, train_size):
     plt.title('Performance comparison on simulation data')
     plt.legend()
   
+    
+def alpha_choice_fig(x, y, my_alphas, nb_features, train_size):
+    '''
+
+    Parameters
+    ----------
+    x : data.
+    y : desired output.
+    my_alphas : array of different values for alpha.
+    nb_features : number of features.
+    train_size : number of train points.
+
+    Returns : representation of lasso path
+    -------
+
+    '''
+    X_train, X_test, y_train, y_test = train_test_split(x, y,train_size=train_size)
+    alpha_for_path, coefs_lasso, _ = lasso_path(X_train[:,0:nb_features-1],
+                                                    X_train[:,nb_features-1],alphas=my_alphas)
+    for i in range(coefs_lasso.shape[0]):
+        plt.plot(alpha_for_path,coefs_lasso[i,:])
+        
+    plt.xlabel('Alpha')
+    plt.ylabel('Coefficients')
+    plt.title('Lasso path')
+    plt.show()
+
+
+def alpha_choice(x, y, my_alphas, nb_features, train_size):
+    '''
+
+    Parameters
+    ----------
+
+    x : data.
+    y : desired output.
+    my_alphas : array of different values for alpha.
+    nb_features : number of features.
+    train_size : number of train points.
+
+    Returns : array of non_zero coefficients depending on alpha
+    ------
+
+    '''
+    X_train, X_test, y_train, y_test = train_test_split(x, y,train_size=train_size)
+    alpha_for_path, coefs_lasso, _ = lasso_path(X_train[:,0:nb_features-1],
+                                                X_train[:,nb_features-1],alphas=my_alphas)   
+    nbNonZero = np.apply_along_axis(func1d=np.count_nonzero,arr=coefs_lasso,axis=0)
+    tab = pd.DataFrame({'alpha':alpha_for_path,'Nb non-zero coefs':nbNonZero})
+    return(tab)
